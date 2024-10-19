fd46c54c97138157344943725256db22f9961d61
Adam Jonas <adamjonas25@gmail.com>
2024-10-19 22:42:18 +0100

    Refactor

---
+++ pointers/wallet_test.go
@@ -0,0 +1,15 @@
+package pointers
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	w := Wallet{}
+	w.Deposit(10)
+
+	got := w.Balance()
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}

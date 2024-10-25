3288a3101ccdd94fa75f1ddca014a6888b7d0cc6
Adam Jonas <adamjonas25@gmail.com>
2024-10-25 21:08:05 +0100

    Handle add error case

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,10 +1,19 @@
 package dictionary
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNoWordFound = errors.New("no word found")
+var (
+	ErrNoWordFound error = DictionaryErr{"no word found"}
+	ErrWordExists  error = DictionaryErr{"word exists"}
+)
+
+type DictionaryErr struct {
+	message string
+}
+
+func (e DictionaryErr) Error() string {
+	return e.message
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	got, exists := d[word]
@@ -12,4 +21,13 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNoWordFound
 	}
 	return got, nil
+}
+
+func (d Dictionary) Add(word string, definition string) error {
+	_, exists := d[word]
+	if exists {
+		return ErrWordExists
+	}
+	d[word] = definition
+	return nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -3,18 +3,39 @@ package dictionary
 import "testing"
 
 func TestDictionary(t *testing.T) {
-	t.Run("happy", func(t *testing.T) {
+	t.Run("happy get", func(t *testing.T) {
 		d := Dictionary{"test": "test def"}
 		got, err := d.Search("test")
 		assert(t, err, nil)
 		assert(t, got, "test def")
 	})
 
-	t.Run("sad", func(t *testing.T) {
+	t.Run("sad get", func(t *testing.T) {
 		d := Dictionary{}
 		got, err := d.Search("test")
 		assert(t, err, ErrNoWordFound)
 		assert(t, got, "")
+	})
+
+	t.Run("happy add", func(t *testing.T) {
+		d := Dictionary{}
+		addErr := d.Add("test", "test def")
+		assert(t, addErr, nil)
+
+		got, searchErr := d.Search("test")
+		assert(t, searchErr, nil)
+		assert(t, got, "test def")
+	})
+
+	t.Run("sad add", func(t *testing.T) {
+		d := Dictionary{}
+		d.Add("test", "test def")
+		addErr := d.Add("test", "new test def")
+		assert(t, addErr, ErrWordExists)
+
+		got, searchErr := d.Search("test")
+		assert(t, got, "test def")
+		assert(t, searchErr, nil)
 	})
 }
 

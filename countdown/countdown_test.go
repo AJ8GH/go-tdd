5e8116204e48246a41f9d9ba51890f09edf33e92
Adam Jonas <adamjonas25@gmail.com>
2024-10-29 22:08:09 +0000

    Test order

--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -3,28 +3,55 @@ package countdown
 import (
 	"bytes"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
-type SpySleeper struct {
-	calls int
+const (
+	write = "write"
+	sleep = "sleep"
+)
+
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.calls++
+func (c *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	c.Calls = append(c.Calls, write)
+	return
+}
+
+func (c *SpyCountdownOperations) Sleep() {
+	c.Calls = append(c.Calls, sleep)
 }
 
 func TestCountdown(t *testing.T) {
-	buffer := bytes.Buffer{}
-	sleeper := SpySleeper{}
-	Countdown(&sleeper, &buffer)
-	got := buffer.String()
-	want := "3\n2\n1\nGo!"
-	assert(t, got, want, "%q")
+	t.Run("happy", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+		Countdown(&SpyCountdownOperations{}, &buffer)
+		got := buffer.String()
+		want := "3\n2\n1\nGo!"
+		assert(t, got, want, "%q")
+	})
 
-	sleepsGot := sleeper.calls
-	sleepsWant := 3
-	assert(t, sleepsGot, sleepsWant, "%d")
+	t.Run("order", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+		got := spySleepPrinter.Calls
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
 
 func assert[T comparable](t testing.TB, got, want T, verb string) {
